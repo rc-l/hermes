b8af69c4cd77d3dec9ced000421cc89f2e3b69aa
Branthalt <24314861+Branthalt@users.noreply.github.com>
2019-02-02 13:01:00 +0100

    created test runner to execute tests in a set order for system testing

---
+++ test_runner.py
@@ -0,0 +1,40 @@
+import unittest
+import sys
+from django.test.utils import setup_test_environment
+from pacioli.tests import TestsModels
+
+
+
+def suiteFactory(
+        *testcases,
+        testSorter   = None,
+        suiteMaker   = unittest.makeSuite,
+        newTestSuite = unittest.TestSuite
+    ):
+    """
+    make a test suite from test cases, or generate test suites from test cases.
+    *testcases     = TestCase subclasses to work on
+    testSorter     = sort tests using this function over sorting by line number
+    suiteMaker     = should quack like unittest.makeSuite.
+    newTestSuite   = should quack like unittest.TestSuite.
+    """
+
+    if testSorter is None:
+        ln         = lambda f:    getattr(tc, f).__code__.co_firstlineno
+        testSorter = lambda a, b: ln(a) - ln(b)
+
+    test_suite = newTestSuite()
+    for tc in testcases:
+        test_suite.addTest(suiteMaker(tc, sortUsing=testSorter))
+
+    return test_suite
+
+def execute():
+    setup_test_environment
+    cases = suiteFactory(TestsModels)
+    runner = unittest.TextTestRunner(verbosity=2)
+    runner.run(cases)
+
+if __name__ == '__main__':
+    print("Execute this using manage.py shell")
+    sys.exit(1)
