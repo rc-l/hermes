56d52195e2fcc6203295c691201834d78875e09b
Branthalt <24314861+Branthalt@users.noreply.github.com>
2019-02-04 20:23:21 +0100

    rewritten tests for pytest which made the test_urunner redundant

--- pacioli/tests.py
+++ pacioli/tests.py
@@ -1,97 +1,64 @@
-from django.test import TestCase
-from django.core.exceptions import ValidationError
-from django.urls import reverse_lazy
+import pytest
 from pacioli import models
 from datetime import date
+from django.test import Client
+from django.core.exceptions import ValidationError
+from django.urls import reverse_lazy
 from bs4 import BeautifulSoup
 import re
 
-class TestsModels(TestCase):
-    def test_accounts_valid(self):
-        account = models.Accounts(name="bank", credit=False, type="AS")
-        self.assertEqual(account.open, date.today())
-        self.assertEqual(account.close, None)
-        try:
-            account.full_clean()
-            clean_success = True
-        except:
-            clean_success = False
-        self.assertTrue(clean_success)
+pytestmark = pytest.mark.django_db
 
-    def test_accounts_invalid_type(self):
-        account = models.Accounts(name="bank", credit=False, type="BS")
-        with self.assertRaisesRegex(
-            ValidationError, "\{'type'\: \[\"Value 'BS' is not a valid choice.\"\]\}"
-        ):
-            account.full_clean()
+data = dict()
+client = Client()
 
-    def test_accounts_no_type(self):
-        account = models.Accounts(name="bank", credit=False)
-        with self.assertRaisesRegex(
-            ValidationError, "\{'type'\: \['This field cannot be blank\.'\]\}"
-        ):
-            account.full_clean()
-
-    def test_accounts_double_name(self):
-        account = models.Accounts(name="bank", credit=False, type="AS")
-        account.save()
-        account2 = models.Accounts(name="bank", credit=True, type="IN")
-        with self.assertRaisesRegex(
-            ValidationError,
-            "\{'name'\: \['Accounts with this Name already exists.'\]\}",
-        ):
-            account2.full_clean()
-
-class DataPersistence():
-    '''
-    Object for storing variables over tests, something that the TestCase does not seem to like.
-    '''
-    pass
-
-class TestsSystem(TestCase):
+## TESTS ##
+def test_create_accounts():
     """
-    All tests in this test case are assumed to be run in the order they are listed below
+    Create accounts both as a test and to supply them to the following tests
     """
-    data = DataPersistence()
+    accounts = {}
+    accounts['bank'] = models.Accounts(name="bank", credit=False, type="AS")
+    assert accounts['bank'].open == date.today()
+    assert accounts['bank'].close == None
 
-    def test_create_accounts(self):
-        """
-        Create accounts both as a test and to supply them to the following tests
-        """
-        accounts = {}
-        accounts['bank'] = models.Accounts(name="bank", credit=False, type="AS")
-        self.assertEqual(accounts['bank'].open, date.today())
-        self.assertEqual(accounts['bank'].close, None)
+    accounts['income'] = models.Accounts(name="income", credit=True, type="AS")
+    data['accounts'] = accounts
 
-        accounts['income'] = models.Accounts(name="income", credit=True, type="AS")
-        self.assertTrue(accounts)
-        self.data.accounts = accounts
-        self.assertTrue(self.data.accounts)
+def test_create_new_transaction():
+    """
+    Create a new transaction.
+    Does both a get and post on a page just like a real user would through the UI
+    """
+    # GET page
+    url = reverse_lazy('new-transaction')
+    ## Check if page is in configuration
+    assert url
+    response = client.get(url)
+    ## Check if page exists
+    assert response.status_code == 200
+    ## Check presence of critical elements on page. Also prevents accidentally confguring for the wrong html template
+    soup = parse_html(response)
+    assert soup.body.find('form', id="transaction_form")
+    assert soup.body.find('button', id="submit-button")
+    # POST data
+    postdata = {'date':date(2018, 12, 31), 'description':'some text'}
+    response = client.post(url, postdata)
+    ## Verify redirect happens to entries page
+    re_test = re.compile("/transactions/[0-9]+/entries")
+    assert response.status_code == 302
+    assert re_test.match(response.get('Location'))
+    data['redirect'] = response.get('Location')
+    ## Verify transaction is created with correct data and that redirect happens to the correct transaction's entries
+    transaction_id = [int(s) for s in response.get('Location').split('/') if s.isdigit()][0]
+    data['transaction_id'] = transaction_id
+    try:
+        transaction = models.Transactions.objects.get(id=transaction_id)
+    except models.Transactions.DoesNotExist:
+        assert False, "Transaction is found not in database"
+    assert transaction.date == postdata.get('date')
+    assert transaction.description == postdata.get('description')
 
-    def test_create_new_transaction(self):
-        """
-        Create a new transaction
-        """
-        # Check present data
-        self.assertTrue(self.data.accounts, "not all the required data is present")
-        # GET page
-        url = reverse_lazy('new-transaction')
-        self.assertTrue(url)
-        response = self.client.get(url)
-        self.assertEquals(response.status_code, 200)
-        soup = BeautifulSoup(response.content, 'html.parser')
-        self.assertTrue(soup.body.find('form', id="transaction_form"))
-        self.assertTrue(soup.body.find('button', id="submit-button"))
-        # POST data
-        postdata = {'date':date(2018, 12, 31), 'description':'some text'}
-        response = self.client.post(url, postdata)
-        re_test = re.compile("/transactions/[0-9]+/entries")
-        self.assertEqual(response.status_code, 302)
-        self.assertTrue(re_test.match(response.get('Location')))
-        transaction_id = [int(s) for s in response.get('Location').split('/') if s.isdigit()][0]
-        try:
-            transaction = models.Transactions.objects.get(id=transaction_id)
-        except models.Transactions.DoesNotExist:
-            self.fail("Transaction is not created")
-        self.assertEquals(transaction.date, postdata.get('date'))
-        self.assertEquals(transaction.description, postdata.get('description'))
+## HELPER CODE ##
+def parse_html(response):
+    return BeautifulSoup(response.content, 'html.parser')
